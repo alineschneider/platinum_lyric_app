664c37e0994377ba1d74036106eec9b583917344
Aline Schneider <alinesviana@hotmail.com>
2020-08-22 12:36:08 -0400

    Adding 'nltk.download('stopwords')' to app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import numpy as np
 import json
 import nltk
 nltk.download('punkt')
+nltk.download('stopwords')
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
